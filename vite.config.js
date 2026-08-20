fec420c45517f1d47f97f84e453a694d9f9ab471
Mary Moor <93663385+MaryCMoor@users.noreply.github.com>
2026-08-20 15:24:56 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Walk-in-My-Shoes-App/',
   plugins: [react()],
   server: {
-    host: "0.0.0.0",
+    host: '0.0.0.0',
     port: 5173,
     strictPort: true,
     allowedHosts: [
-      ".apps.run.brev.nvidia.com"
+      '.apps.run.brev.nvidia.com'
     ]
   }
 })
